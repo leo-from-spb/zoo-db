1346a35b38bce9763fbecbd4759dd7a5a723cecb
Leonid Bushuev <leonidos@gmail.com>
2025-06-13 13:56:17 +0200

    Microsoft: fulltext indices (improvement)

--- DB/Microsoft/Zoo-25/Menagerie/create-Fulltext.sql
+++ DB/Microsoft/Zoo-25/Menagerie/create-Fulltext.sql
@@ -1,3 +1,7 @@
+use Menagerie
+go
+
+
 create fulltext catalog Ftx_Default as default
 go
 
@@ -10,10 +14,11 @@ create table Zoo.Ftx_Tab_1 (
 create table Zoo.Ftx_Tab_2 (
   Id int not null
       constraint Ftx_Tab_2_pk primary key,
-  Note nvarchar(1000)
+  Note nvarchar(1000),
+  Remark nvarchar(160)  
 )
 
 create fulltext index on Zoo.Ftx_Tab_1 (Note) key index Ftx_Tab_1_pk;
-create fulltext index on Zoo.Ftx_Tab_2 (Note) key index Ftx_Tab_2_pk;
+create fulltext index on Zoo.Ftx_Tab_2 (Note, Remark) key index Ftx_Tab_2_pk;
 go
 
